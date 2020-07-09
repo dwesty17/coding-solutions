abfe4f94096956c59ec16c8ba09d732ea8bc8470
dwesty17 <j.dylan.westerhold@gmail.com>
2020-07-09 17:45:51 -0400

    Added solution for `shuffle-an-array` LeetCode problem

---
+++ leetcode-solutions/shuffle-an-array/shuffle-an-array.test.js
@@ -0,0 +1,8 @@
+const { Solution } = require("./shuffle-an-array");
+
+describe("Solution", () => {
+  test("Example test case 1", () => {
+    const solution = new Solution([1, 2, 3])
+    // TODO not sure how to test this one yet?
+  });
+});
